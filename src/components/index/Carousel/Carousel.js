1ea79963b557353cf01d0905311253c11c3c317d
tomasmanueltm <tomasmanuelbaptistabaptista@gmail.com>
2022-10-18 14:48:53 +0100

    casousel

--- src/components/index/Carousel/Carousel.js
+++ src/components/index/Carousel/Carousel.js
@@ -1,4 +1,5 @@
 import "./Carousel.css";
+import logo from "../../../logo.svg";
 
 
 
@@ -7,11 +8,21 @@ function Carousel()
 {   
     return (
         <div className="carousel">
-            <img src="" alt="arquivo" />
+            <img src={logo} alt="arquivo" />
 
             <div className="legenda">
                 <h1>Informação</h1>
                 <p>paragrafo</p>
+
+                <div className="form-search">
+                    <input className="input" placeholder="Busca"></input>   
+                    <svg class="svg-icon" viewBox="0 0 19.9 19.7">
+                        <g class="search-path" fill="none" stroke="#777">
+                            <path stroke-linecap="square" stroke-width="3" d="M18.5 18.3l-5.4-5.4"/>
+                            <circle cx="8" cy="8" r="7" stroke-width="3" />
+                        </g>
+                    </svg>
+                </div>
             </div>
         </div> 
     );
